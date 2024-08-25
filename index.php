8e21235c27bbdce0bb2a0bd4b29b352e8eca4cc9
wuan25 <pquan9939@gmail.com>
2024-08-26 03:07:07 +0700

    update

--- index.php
+++ index.php
@@ -7,10 +7,48 @@ require_once './Globals.php';
 if (isset($_GET['act'])) {
     $act = $_GET['act'];
     switch ($act) {
-        case 'value':
-            # code...
+        case 'register':
+            # chức năng đăng ký
             break;
-
+        case 'logIn':
+            # chức năng đăng nhập
+            break;
+        case 'logOut':
+            # chức năng đăng xuất
+            break;
+        case 'genre':
+            # loại phim
+            break;
+        case 'film':
+            # phim
+            break;
+        case 'showTimes':
+            # lịch chiếu
+            break;
+        case 'moviesShowing':
+            # phim đang chiếu
+            break;
+        case 'upcomingMovies':
+            # phim sắp chiếu
+            break;
+        case 'cinema':
+            # rạp chiếu 
+            break;
+        case 'movieReview':
+            # review phim
+            break;
+        case 'topMovies':
+            # top phim
+            break;
+        case 'movieBlog':
+            # blog phim
+            break;
+        case 'search':
+            # tim kiếm
+            break;
+        case 'movieDetails':
+            # chi tiết phim
+            break;
         default:
             # code...
             break;
