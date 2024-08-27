f5e93f320cebb525f398a10b92204f6d9f804244
wuan25 <pquan9939@gmail.com>
2024-08-27 20:24:56 +0700

    chức năng đăng ký

---
+++ Models/taikhoan.php
@@ -0,0 +1,15 @@
+<?php
+#thêm tài khoản
+function insertAccount($ten_dang_nhap, $ho_va_ten, $email, $phone, $matkhau, $vaitro, $trangthai)
+{
+    $sql = "INSERT INTO taikhoan(ten_dang_nhap,ho_va_ten,email,sdt,mat_khau,vai_tro,trang_thai) 
+VALUES('$ten_dang_nhap','$ho_va_ten','$email','$phone','$matkhau','$vaitro','$trangthai')";
+    pdo_execute($sql);
+}
+#check tài khoản
+function checkAccount($ten_dang_nhap, $ho_va_ten, $email)
+{
+    $sql = "SELECT * FROM taikhoan WHERE ten_dang_nhap = '$ten_dang_nhap' OR ho_va_ten = '$ho_va_ten' OR email = '$email'";
+    $check = pdo_query_one($sql);
+    return $check;
+}
--- index.php
+++ index.php
@@ -1,12 +1,67 @@
 <?php
 session_start();
+ob_start();
 require_once './Models/pdo.php';
 require_once './Views/header.php';
+require_once './Models/taikhoan.php';
 # Xử lý Swich case.
 if (isset($_GET['act'])) {
     $act = $_GET['act'];
     switch ($act) {
         case 'register':
+            $error = $erUsername = $ername = $erEmail = $erPhone = $erPassword = "";
+            $erCount = 0;
+            if (isset($_POST['btn_signup'])) {
+                $username = $_POST['username'];
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $phone = $_POST['phone'];
+                $password = $_POST['password'];
+                $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+                $passwordPattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
+                $trangthai = 0;
+                $vaitro = 0;
+                #validate
+                if (empty($username)) {
+                    $erUsername = "Không được để trống username";
+                    $erCount++;
+                }
+                if (empty($name)) {
+                    $erName = "Không được để trống name";
+                    $erCount++;
+                }
+                if (empty($email)) {
+                    $erEmail = "Không được để trống email";
+                    $erCount++;
+                }
+                if (empty($phone)) {
+                    $erPhone = "Không được để trống phone";
+                    $erCount++;
+                }
+                if (empty($password)) {
+                    $erPassword = "Không được để trống password";
+                    $erCount++;
+                }
+                if (!preg_match($passwordPattern, $password)) {
+                    $erPassword = "Mật khẩu phải ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số.";
+                    $erCount++;
+                }
+                #đăng ký
+                if ($erCount == 0) {
+                    #check tài khoản
+                    $check = checkAccount($username, $name, $email);
+                    if ($check) {
+                        $error = "Tài khoản này đã tồn tại!";
+                    } else {
+                        insertAccount($username, $name, $email, $phone, $passwordHash, $vaitro, $trangthai);
+                        echo "<script>
+                        alert('Đăng ký thành công!');
+                        window.location.href = 'index.php?act=logIn';
+                        </script>";
+                        exit();
+                    }
+                }
+            }
             require_once './Views/Account/register.php';
             break;
         case 'logIn':
@@ -49,10 +104,11 @@ if (isset($_GET['act'])) {
             # chi tiết phim
             break;
         default:
-        require_once './Views/main.php';
+            require_once './Views/main.php';
             break;
     }
-}else{
+} else {
     require_once './Views/main.php';
 }
 require_once './Views/footer.php';
+ob_end_flush();
